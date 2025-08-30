d04fcc115c9fdd8599ddb23c3eedc871241c6a50
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2025-08-30 01:36:38 -0400

    feat: add queries/highlights.scm for simplified grammar
    - Only references node types that exist: comment, variable, identifier, value
    - No references to bool, url, integer etc which don't exist in simplified grammar
    - Fixes Zed extension loading error

---
+++ queries/highlights.scm
@@ -0,0 +1,12 @@
+; Comments
+(comment) @comment
+
+; Variable names (keys)
+(variable
+  name: (identifier) @variable.parameter)
+
+; All values as strings
+(value) @string
+
+; Operators
+"=" @operator
